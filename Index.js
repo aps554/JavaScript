9b4f6173a22d8cccc7b57532f81c6d9057b93d10
Your Name <amitpratapsingh0000@gmail.com>
2020-09-16 22:30:44 +0530

    Previous Updates

--- Index.js
+++ Index.js
@@ -1,1 +1,15 @@
 console.log("Hello World");
+
+let Age = 24;
+console.log(Age);
+
+let firstName, lastName = "Singh";
+
+// declaring multiple varialbles
+console.log(firstName);
+console.log(lastName);
+
+// constants
+const pi = 3.414
+
+console.log(pi)
