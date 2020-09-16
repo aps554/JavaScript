84b76b0fc4e2b3531d7e4c9e1b8acc0975797e53
Your Name <amitpratapsingh0000@gmail.com>
2020-09-17 01:26:06 +0530

    Primitive data type

--- Index.js
+++ Index.js
@@ -10,6 +10,36 @@ console.log(firstName);
 console.log(lastName);
 
 // constants
-const pi = 3.414
+// always keep in capital letter
+const PI = 3.414;
+console.log(PI);
 
-console.log(pi)
+
+// data types
+// Primitive data types are all immutable in nature
+
+let age = 52;
+console.log(age);
+age = "skjef" + "kjfad" + 54;
+
+// string not concatinated but added as immutable
+console.log(age);
+
+
+//String 
+// Boolean
+let Do=true ;
+
+// Symbols
+
+const blue = Symbol();
+
+const white = Symbol("White");
+
+
+console.log(blue===white)
+
+
+// Null and notdefined
+
+// OBJECT TYPE
