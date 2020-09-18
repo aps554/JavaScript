920d0c551ec87bd3b87ba420416f7b2bd92efe65
Your Name <amitpratapsingh0000@gmail.com>
2020-09-19 02:07:12 +0530

    media and inputs

---
+++ FRONTEND js/script.js
@@ -0,0 +1,23 @@
+// alert('chutiya hai kya')
+
+console.log("ha hai kr le jo krna hai")
+
+// variables
+
+let x = 25
+let y = "kjghkhg"
+let z = true
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+console.log('x is ',typeof x)
+console.log('y is ',typeof y)
+console.log('z is ',typeof z)
+
+function sayHello(Name) {
+    console.log('Hello '+Name);
+}
+
+let helloBtn = document.getElementById("helloBtn")
