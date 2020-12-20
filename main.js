485b4141e3364671f91874b3140d48f1dc1dd4a7
Your Name <amitpratapsingh0000@gmail.com>
2020-12-20 23:20:37 +0530

    moving to advanced topics

--- main.js
+++ main.js
@@ -16,3 +16,25 @@ const rollNo = 5;
 // identifier
 // starts with $ or _
  
+const obj = {
+    f1 : "Amit",
+    f2 : "Pratap",
+    f3 : "Singh",
+    f4 : "From DTU",
+    
+
+}
+
+for (let f in obj){
+    console.log(`${f} is ${obj[f]}`);
+}
+
+console.log("\n");
+console.log("\n");
+console.log("\n");
+
+const obj1 = {};
+const y = obj1;
+y.d = "Property created";
+
+console.log(obj1);
