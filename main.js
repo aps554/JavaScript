2b57e8d09459389eada0542b3a24c1ab357f2eed
Your Name <amitpratapsingh0000@gmail.com>
2020-12-20 23:30:45 +0530

    class adding functions and properties

--- main.js
+++ main.js
@@ -38,3 +38,45 @@ const y = obj1;
 y.d = "Property created";
 
 console.log(obj1);
+
+
+console.log("\n");
+console.log("\n");
+console.log("\n");
+
+
+/**
+ * Class : syntax
+ * class model{
+ *  constructor(){
+ *      
+ * 
+ *  }
+ * }
+*/
+
+class Model{
+    constructor(Mno,Mname){
+        // console.log("object created");
+        this.Mno = Mno;
+        this.Mname = Mname;
+    }
+
+    show(){
+        console.log(`Model No: ${this.Mno}
+Model Name: ${this.Mname}`);
+    }
+}
+
+const obj3 = new Model(1,"Dzire");
+const obj2 = new Model(2,"Swift");
+
+
+console.log(obj3 instanceof Model);
+console.log(obj1 instanceof Model);
+console.log(obj1 instanceof Object);
+
+console.log(obj2);
+console.log(obj3);
+
+obj2.show();
