29041a9f83070531990f9f53c85e15fa4413d30b
Your Name <amitpratapsingh0000@gmail.com>
2020-12-22 22:14:51 +0530

    attributes with any tags

---
+++ dataattribute.js
@@ -0,0 +1,23 @@
+
+
+let nav = document.querySelectorAll(".navbar");
+
+
+console.log(nav[1].getAttribute("data-name"));
+console.log(nav[1].getAttribute("data-number"));
+
+console.log(nav[0].dataset.name);
+console.log(nav[0].dataset.number);
+
+
+let para = document.querySelector(".two");
+
+let color = function(flag){
+    if(flag)
+        para.className = "togle_in";
+    else
+        para.className = "togle_out";
+}
+
+
+color(false);
