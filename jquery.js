5a109ee25b7bee202fe2185934b4bb4d17e51962
Your Name <amitpratapsingh0000@gmail.com>
2020-12-23 00:50:22 +0530

    using jquery functions and importing that

---
+++ jquery.js
@@ -0,0 +1,12 @@
+
+
+let para = $(".two");
+para.innerHTML = "Two para";
+para[1].style.color = "red";
+console.log(para[1]);
+
+let para2 = $("<p class = 'two'>Add some text here</p>");
+let div = $(".one");
+
+div[0].appendChild(para2[0]);
+console.log(div[0]);
