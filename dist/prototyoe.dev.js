02b75bd7b9fbad0080e88997b9f256eced859b49
Your Name <amitpratapsingh0000@gmail.com>
2021-06-03 13:44:13 +0530

    'update'

--- dist/prototyoe.dev.js
+++ dist/prototyoe.dev.js
@@ -1,26 +1,21 @@
+"use strict";
+
 /**
  * Prototype mai tb dekhi jb object ke pass vo propert na ho
  * prefer properties specific to obj
  *
 */
-
-let f  = function(){
-    this.a = 1;
-    // this.b = 2;
+var f = function f() {
+  this.a = 1; // this.b = 2;
 };
 
-const obj = new f();
-
+var obj = new f();
 obj.c = 5;
-
 f.prototype.b = 3;
 f.prototype.d = 4;
-
-
 console.log(obj.a);
 console.log(obj.b);
 console.log(obj.d);
 console.log(f.prototype.b);
-
 console.log(f);
 console.log(f.prototype);
